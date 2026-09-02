6a4009402a5b22631ee186b4ade28412b6d38880
CtrlLuzzio <luis.montilla.ag@outlook.com>
2026-09-02 15:07:27 -0400

    feat: Made basic styling and base layout, header and added fonts
    Text in pages is for testing purpouses

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,28 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-
+import { defineConfig, fontProviders } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+  fonts: [
+    {
+      name: "Outfit",
+      cssVariable: "--font-sans",
+      provider: fontProviders.fontsource(),
+      weights: ["200 800"],
+      styles: ["normal"],
+      fallbacks: ["sans-serif"]
+    },
+    {
+      name: "JetBrains Mono",
+      cssVariable: "--font-mono",
+      provider: fontProviders.fontsource(),
+      weights: ["100 800"],
+      styles: ["normal", "italic"],
+      fallbacks: ["monospace"]
+    }
+  ]
 });
