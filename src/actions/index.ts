6dbe8a9661c7b0595367b1dc452b9c5c06a473a5
CtrlLuzzio <luis.montilla.ag@outlook.com>
2026-09-10 10:26:59 -0400

    feat: Modularized actions

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,39 +1,5 @@
-import { defineAction } from "astro:actions";
-import { z } from "astro:schema";
-
-let cachedStatus = { isLive: false, lastCheck: 0 };
-const CACHE_TTL = 60000;
+import { twitch } from "./twitch";
 
 export const server = {
-    getTwitchStatus: defineAction({
-        handler: async () => {
-            const now = Date.now()
-
-            if (now - cachedStatus.lastCheck < CACHE_TTL) {
-                return { isLive: cachedStatus.isLive };
-            }
-
-            try {
-                const clientId = import.meta.env.TWITCH_CLIENT_ID;
-                const clientSecret = import.meta.env.TWITCH_CLIENT_SECRET;
-                const username = import.meta.env.TWITCH_USERNAME;
-
-                // TODO
-                // 1. Fetch https://id.twitch.tv/oauth2/token to get App Access Token.
-                // 2. Fetch https://api.twitch.tv/helix/streams?user_login={username}
-                // 3. Evaluate if data comes empty (offline) or not (online)
-
-                // temp sim to test visuals and classes
-
-                const isLive = true;
-
-                cachedStatus = { isLive, lastCheck: now };
-
-                return { isLive };
-            } catch (error) {
-                console.error("Error fetching twitch status", error)
-                return { isLive: false };
-            }
-        }
-    })
-}
+    twitch,
+};
